e430432fdde9c3a7893e071ce5fcf4f86031d18a
岩浆块Magma <magmablock@qq.com>
2025-07-08 19:09:34 +0800

    init

---
+++ src/main/java/ink/magma/zthMyMenu/ZthMyMenu.java
@@ -0,0 +1,17 @@
+package ink.magma.zthMyMenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZthMyMenu extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
